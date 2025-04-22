43ae16129f10f038407c623c0240e73bae7b1ee7
Abhigyan126 <abhigyanpandey.cug@gmail.com>
2025-04-22 12:02:55 +0530

    issue #46 : implemented diseaseCraft

--- src/front/medsim-ai-front/src/App.js
+++ src/front/medsim-ai-front/src/App.js
@@ -6,6 +6,8 @@ import Logout from './modules/routes/logout';
 import Profile from './modules/routes/profile';
 import Simulator from './modules/routes/symptom';
 import About from './modules/routes/about';
+import DiseaseCraft from './modules/routes/diseaseCraft';
+
 function App() {
   return (
     <Router>
@@ -16,6 +18,7 @@ function App() {
         <Route path='/logout' element={<Logout />} />
         <Route path='/profile' element={<Profile />} />
         <Route path='/about' element={<About />} />
+        <Route path='/diseaseCraft' element={<DiseaseCraft />} />
         <Route path='/symptom-simulator' element={<Simulator />} />
         </Routes>
     </Router>
