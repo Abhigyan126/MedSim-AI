c3a7fde42f371e0cc40fe2a295daa2d7aa103c48
Abhigyan126 <abhigyanpandey.cug@gmail.com>
2025-04-23 21:40:19 +0530

    fixed head merge error

--- src/front/medsim-ai-front/src/App.js
+++ src/front/medsim-ai-front/src/App.js
@@ -7,11 +7,8 @@ import Profile from './modules/routes/profile';
 import Simulator from './modules/routes/symptom';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import About from './modules/routes/about';
-<<<<<<< HEAD
-=======
 import DiseaseCraft from './modules/routes/diseaseCraft';
 import SymptomTree from './modules/routes/symptomtree';
->>>>>>> 4210440d5ddf95543f5b0819c36a3462642b7b5e
 
 function App() {
   return (
